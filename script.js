e08590b76e031ab9267dffdbd614d3f8fa160ca3
Vijay <vijay.saratkar@techiegent.com>
2024-06-25 17:01:22 +0530

    feat:added conteact form and remove un used code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
   });
-  
+
 });
